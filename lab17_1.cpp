7bc3674c6c727fb91270f3db0d803238d69eb567
Aungpao <aungpao321@gmail.com>
2020-03-13 17:06:06 +0700

    1

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -21,24 +21,18 @@ int main(){
     char name[100];
     while(getline(f,s)){
         sscanf(s.c_str(),"%[^:]: %d %d %d",name,&num1,&num2,&num3);
-        cout<<name<<" : ";
         v_name.push_back(name);
         sum=num1+num2+num3;
         if(sum>=80){
             v_grade.push_back("A");
-            cout<<"A\n";
         }else if(sum>=70){
             v_grade.push_back("B");
-            cout<<"B\n";
         }else if(sum>=60){
             v_grade.push_back("C");
-            cout<<"C\n";
         }else if(sum>=50){
             v_grade.push_back("D");
-            cout<<"D\n";
         }else{
             v_grade.push_back("F");
-            cout<<"F\n";
         }
     }
 
@@ -48,19 +42,35 @@ int main(){
         int idx=s.find_first_of(" ");
         string s1=upper(s.substr(0,idx));
         string s2=upper(s.substr(idx+1,s.size()));
-        
+        bool v;
         if(s1=="NAME"){
+            v=1;
             for(int i=0; i<v_name.size(); i++){
                 if(s2==upper(v_name[i])){
+                    cout<<"-----------------------------\n";
                     cout<<v_name[i]<<"'s grade = "<<v_grade[i]<<"\n";
+                    cout<<"-----------------------------\n";
+                    v=0;
                 }
             }
+            if(v !=0){
+                cout<<"-----------------------------\n";
+                cout<<"Cannot Found.\n";
+                cout<<"-----------------------------\n";
+            }
         }else if(s1=="GRADE"){
+            v=1;
+            cout<<"-----------------------------\n";
             for(int i=0; i<v_grade.size(); i++){
                 if(s2==upper(v_grade[i])){
                     cout<<v_name[i]<<"\n";
+                    v=0;
                 }
             }
+            if(v!=0){
+                cout<<"Cannot Found.\n";
+            }
+            cout<<"-----------------------------\n";
         }else if(s1=="EXIT"){
             break;
         }else cout<<"Invalid command\n";
